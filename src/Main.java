4856cf8a0f0c39e106959f183273486555cfede1
R. Cem Kahveci <88493840+khvci@users.noreply.github.com>
2022-02-16 15:13:26 +0300

    remove indexes while adding items to arraylist

--- src/Main.java
+++ src/Main.java
@@ -55,16 +55,16 @@ public class Main {
         ArrayList<String> linkList = new ArrayList<String>();
 
         if (group.equals("followers")) {
-            linkList.add(0, "https://github.com/khvci?page=1&tab=followers");
-            linkList.add(1, "https://github.com/khvci?page=2&tab=followers");
-            linkList.add(2, "https://github.com/khvci?page=3&tab=followers");
-            linkList.add(3, "https://github.com/khvci?page=4&tab=followers");
-            linkList.add(4, "https://github.com/khvci?page=5&tab=followers");
+            linkList.add("https://github.com/khvci?page=1&tab=followers");
+            linkList.add("https://github.com/khvci?page=2&tab=followers");
+            linkList.add("https://github.com/khvci?page=3&tab=followers");
+            linkList.add("https://github.com/khvci?page=4&tab=followers");
+            linkList.add("https://github.com/khvci?page=5&tab=followers");
         } else {
-            linkList.add(0, "https://github.com/khvci?page=1&tab=following");
-            linkList.add(1, "https://github.com/khvci?page=2&tab=following");
-            linkList.add(2, "https://github.com/khvci?page=3&tab=following");
-            linkList.add(3, "https://github.com/khvci?page=4&tab=following");
+            linkList.add("https://github.com/khvci?page=1&tab=following");
+            linkList.add("https://github.com/khvci?page=2&tab=following");
+            linkList.add("https://github.com/khvci?page=3&tab=following");
+            linkList.add("https://github.com/khvci?page=4&tab=following");
             //linkList.add(4, "https://github.com/khvci?page=5&tab=following");
         }
 
