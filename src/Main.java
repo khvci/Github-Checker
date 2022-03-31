a7d444c8d559437b8492658407517d637270f6dc
khvci <88493840+khvci@users.noreply.github.com>
2022-03-31 22:52:25 +0300

    update Main.java

--- src/Main.java
+++ src/Main.java
@@ -55,7 +55,7 @@ public class Main {
             linkList.add("https://github.com/khvci?page=2&tab=followers");
             linkList.add("https://github.com/khvci?page=3&tab=followers");
             linkList.add("https://github.com/khvci?page=4&tab=followers");
-            linkList.add("https://github.com/khvci?page=5&tab=followers");
+            //linkList.add("https://github.com/khvci?page=5&tab=followers");
         } else {
             linkList.add("https://github.com/khvci?page=1&tab=following");
             linkList.add("https://github.com/khvci?page=2&tab=following");
