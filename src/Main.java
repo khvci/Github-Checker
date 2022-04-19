6a75a39292fcf2c134407388de8aca0a20682ebc
khvci <88493840+khvci@users.noreply.github.com>
2022-04-20 00:42:37 +0300

    simplify main method

--- src/Main.java
+++ src/Main.java
@@ -4,37 +4,10 @@ import java.io.InputStream;
 import java.util.ArrayList;
 
 public class Main {
-    static long startTime = System.nanoTime();
-    static String userName = "khvci";
-    static ArrayList<String> followers = new ArrayList<>();
-    static ArrayList<String> following = new ArrayList<>();
-
     public static void main(String[] args) throws IOException {
-        TxtFileManager txtFileManager = new TxtFileManager();
-        txtFileManager.createTxtFile("followers.txt");
-        txtFileManager.createTxtFile("following.txt");
-
-        InputStream followerStream = new FileInputStream("src/followers.txt");
-        InputStream followingStream = new FileInputStream("src/following.txt");
-
-        SourceReader sourceReader = new SourceReader();
-        sourceReader.readSource(userName, 5, "followers");
-        sourceReader.readSource(userName, 4, "following");
-
-        ArrayBuilder arrayBuilder = new ArrayBuilder();
-        arrayBuilder.arrayBuilder(followers, followerStream);
-        arrayBuilder.arrayBuilder(following, followingStream);
-
-        DifferenceFinder differenceFinder = new DifferenceFinder();
-        differenceFinder.findDifference(followers, following);
-
-        ResultPrinter resultPrinter = new ResultPrinter();
-        resultPrinter.printResults(startTime, followers, following,
-                differenceFinder.dontFollowYou, differenceFinder.youDontFollow);
-
-        txtFileManager.deleteTxtFile("followers.txt");
-        txtFileManager.deleteTxtFile("following.txt");
-
-        System.out.println("\nTotal runtime: " + (System.nanoTime() - startTime) / 1000000 + " ms.");
+        GithubChecker githubChecker = new GithubChecker();
+        githubChecker.checkGithub("khvci", 5, 4);
     }
+
+
 }
