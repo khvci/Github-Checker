a96979984546b9fa8dc17dc6475e50ed856f73f4
khvci <88493840+khvci@users.noreply.github.com>
2024-02-23 18:30:34 +0300

    fix number fetcher method

--- src/tools/UserNumbersGetter.java
+++ src/tools/UserNumbersGetter.java
@@ -19,18 +19,19 @@ public class UserNumbersGetter {
         profilePageSourceCode.append(scanner.next());
 
         String subString1 = profilePageSourceCode.substring(
-                profilePageSourceCode.indexOf("</svg>\n          ") + 58,
+                profilePageSourceCode.indexOf("</svg>\n" +
+                        "          <span class=\"text-bold color-fg-default\">") + 58,
                 profilePageSourceCode.indexOf("</span>\n          followers"));
-
 
         String subString2 = profilePageSourceCode.substring(
                 profilePageSourceCode.indexOf("?tab=following\">\n          <span class=\"text-bold color-fg-default\">") + 68,
                 profilePageSourceCode.indexOf("</span>\n          following"));
 
-
         try {
+
             Main.setFollowersPageNumber((Integer.parseInt(subString1) / 50 + 1));
         } catch (Exception ex) {
+            System.out.println("ERROR: substring1: "+subString1);
             String[] temp1 = subString1.split("\\.");
             subString1 = temp1.length < 2 ?
                     temp1[0].split("k")[0] + "000" :
@@ -40,9 +41,10 @@ public class UserNumbersGetter {
         }
 
         try {
+
             Main.setFollowingPageNumber((Integer.parseInt(subString2) / 50 + 1));
         } catch (Exception ex) {
-
+            System.out.println("ERROR: substring2: "+subString2);
             String[] temp2 = subString2.split("\\.");
             subString2 = temp2.length < 2 ?
                     temp2[0].split("k")[0] + "000" :
