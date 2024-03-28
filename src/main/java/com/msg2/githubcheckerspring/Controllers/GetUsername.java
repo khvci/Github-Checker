e5020901ff34eac7c97d3a07c93bfff6ae8910bd
khvci <88493840+khvci@users.noreply.github.com>
2024-03-28 16:31:29 +0300

    extract method from controller to manager class

--- src/main/java/com/msg2/githubcheckerspring/Controllers/GetUsername.java
+++ src/main/java/com/msg2/githubcheckerspring/Controllers/GetUsername.java
@@ -1,9 +1,7 @@
 package com.msg2.githubcheckerspring.Controllers;
 
-import org.springframework.core.io.ClassPathResource;
+import com.msg2.githubcheckerspring.Managers.UsernameManager;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,14 +11,9 @@ public class GetUsername {
 
     @GetMapping("/github-checker/")
     public ResponseEntity<Resource> askUserName() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.TEXT_HTML);
-
-        Resource resource = new ClassPathResource("templates/form.html");
-
         return ResponseEntity.ok()
-                .headers(headers)
-                .body(resource);
+                .headers(UsernameManager.getHttpHeaders())
+                .body(UsernameManager.getUsername());
     }
 }
 
---
+++ src/main/java/com/msg2/githubcheckerspring/Managers/UsernameManager.java
@@ -0,0 +1,17 @@
+package com.msg2.githubcheckerspring.Managers;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+public class UsernameManager {
+    public static Resource getUsername() {
+        return new ClassPathResource("templates/form.html");
+    }
+    public static HttpHeaders getHttpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_HTML);
+        return headers;
+    }
+}
