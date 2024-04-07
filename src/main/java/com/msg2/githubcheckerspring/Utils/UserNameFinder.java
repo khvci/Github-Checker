574e288d0333a43d4fd02a82e794ac22eb9d2cf0
khvci <88493840+khvci@users.noreply.github.com>
2024-04-07 14:35:15 +0300

    add UserNameFinder class

---
+++ src/main/java/com/msg2/githubcheckerspring/Utils/UserNameFinder.java
@@ -0,0 +1,15 @@
+package com.msg2.githubcheckerspring.Utils;
+
+public class UserNameFinder {
+    public static String getUserName(String str) {
+        return str
+                .split("user\" data-hovercard-url=\"/users/")[1]
+                .split("/hovercard\" data-octo-click=")[0];
+    }
+
+    public static String getCompanyName(String str) {
+        return str
+                .split("data-hovercard-url=\"/orgs/")[1]
+                .split("/hovercard\" data-octo-click")[0];
+    }
+}
